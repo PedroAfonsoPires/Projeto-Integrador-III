c236653dac3020de3f0e890b5b1cd738fd7ab5b0
ThiagoPfeifer09 <166036070+ThiagoPfeifer09@users.noreply.github.com>
2024-11-22 18:08:55 -0300

    Add files via upload

---
+++ Thiago/main.py
@@ -0,0 +1,16 @@
+import analisador_lexico as lex
+import analisador_sintatico as sin
+
+# Código de exemplo em C (para ser processado pelo analisador léxico e sintático)
+name = "quick_sort.c"
+with open(name, 'r', encoding="utf-8") as file_:
+    c_code = file_.read()
+
+# Processar o código de entrada com o analisador léxico
+print("Processando o código com o analisador léxico...")
+lex.process_code(c_code, 0)
+
+# Executar o analisador sintático
+print("Analisando o código com o analisador sintático...")
+ast = sin.parse_code(c_code)
+print("Análise concluída.")
