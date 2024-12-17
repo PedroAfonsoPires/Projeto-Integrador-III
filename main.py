9643f1ccba291c5cd5f8e4f93c36bcf0e7f2bbb8
OtavioFarias <137441117+OtavioFarias@users.noreply.github.com>
2024-12-16 22:52:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import analisador_lexico as lex
 import analisador_sintatico as sin
 import geradorIntermediario as gi
-
+import analisador_semantico as ase
 # Código de exemplo em C (para ser processado pelo analisador léxico e sintático)
 name = "quick_sort.c"
 with open(name, 'r', encoding="utf-8") as file_:
@@ -16,9 +16,18 @@ print("Analisando o código com o analisador sintático...")
 ast = sin.parse_code(c_code, 0)
 print("Análise concluída.")
 
+# Criando o objeto SemanticAnalyzer
+analyzer = ase.SemanticAnalyzer()
+
+# Chamando o método analyze com a AST
+analyzer.analyze(ast)
+
+# Gerando o relatório de símbolos e erros
+analyzer.generate_report()
+
 # Gerar código intermediário
-print("Gerador do Código Intermediário...")
-codI = gi.process_node(ast)
-print("Código Intermediário Gerado:")
-print(codI)
-print("Geração concluída.")
+#print("Gerador do Código Intermediário...")
+#codI = gi.process_node(ast)
+#print("Código Intermediário Gerado:")
+#print(codI)
+#print("Geração concluída.")
