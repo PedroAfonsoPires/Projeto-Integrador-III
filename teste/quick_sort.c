70a1253a7ccf5249aec8c6e336052e8d368820f5
OtavioFarias <137441117+OtavioFarias@users.noreply.github.com>
2024-12-17 13:02:36 -0300

    Create quick_sort.c

---
+++ teste/quick_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 20;
+
+    // Declaração com atribuição
+    float z = 0;
+    float w[];
+    // If statement
+    if (x < y) {
+        z = x + y;
+    }
+
+    // If-else statement
+    if (z > 30) {
+        i++;
+    } else {
+        i++;
+    }
+
+    // While loop
+    int counter = 0;
+    while (counter < 5) {
+        i++;
+        counter++;
+    }
+
+    // Do-while loop
+    int count_down = 5;
+    do {
+        i++;
+        count_down--;
+    } while (count_down > 0);
+
+    // For loop
+    for (int i = 0; i < 5; i++) {
+        i++;
+    }
+    return 0;
+}
