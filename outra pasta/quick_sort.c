69bd40f3c2c43c00088622a6096fb8626562ebb9
OtavioFarias <137441117+OtavioFarias@users.noreply.github.com>
2024-12-18 12:43:47 -0300

    Update quick_sort.c

--- outra pasta/quick_sort.c
+++ outra pasta/quick_sort.c
@@ -1,41 +1,59 @@
 #include <stdio.h>
 
+// Função para trocar dois elementos
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Particiona o array e retorna o índice do pivô
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // Escolhe o último elemento como pivô
+    int i = (low - 1); // Índice do menor elemento
+
+    for (int j = low; j < high; j++) {
+        // Se o elemento atual é menor ou igual ao pivô
+        if (arr[j] <= pivot) {
+            i++; // Incrementa o índice do menor elemento
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]); // Coloca o pivô na posição correta
+    return (i + 1);
+}
+
+// Função principal do Quicksort
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        // Particiona o array e obtém o índice do pivô
+        int pi = partition(arr, low, high);
+
+        // Ordena recursivamente as duas metades
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+
+// Função para imprimir o array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Função principal
 int main() {
-    int x = 10;
-    int y = 20;
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    // Declaração com atribuição
-    float z = 0;
-    float w[];
-    // If statement
-    if (x < y) {
-        z = x + y;
-    }
+    printf("Array original: ");
+    printArray(arr, n);
 
-    // If-else statement
-    if (z > 30) {
-        i++;
-    } else {
-        i++;
-    }
+    quicksort(arr, 0, n - 1);
 
-    // While loop
-    int counter = 0;
-    while (counter < 5) {
-        i++;
-        counter++;
-    }
-
-    // Do-while loop
-    int count_down = 5;
-    do {
-        i++;
-        count_down--;
-    } while (count_down > 0);
-
-    // For loop
-    for (int i = 0; i < 5; i++) {
-        i++;
-    }
+    printf("Array ordenado: ");
+    printArray(arr, n);
     return 0;
 }
