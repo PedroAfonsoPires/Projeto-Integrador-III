858bb318e05eaa06be832aaaadb13d7d7d3fe45c
OtavioFarias <137441117+OtavioFarias@users.noreply.github.com>
2024-12-18 22:51:39 -0300

    Update main.py

--- outra pasta/main.py
+++ outra pasta/main.py
@@ -11,7 +11,7 @@ with open(name, 'r', encoding="utf-8") as file_:
 
 # Processar o código de entrada com o analisador léxico
 print("Processando o código com o analisador léxico...")
-lex.process_code(c_code, 0)
+lex.process_code(c_code, 1)
 
 print()
 
@@ -26,16 +26,16 @@ print()
 print("Executando o analisador semântico...")
 semantic_analyzer = sem.SemanticAnalyzer()  # Inicializa o analisador semântico
 try:
-    print(ast)
     semantic_analyzer.analyze(ast)  # Realiza a análise semântica na AST
     print("Análise Semântica concluída sem erros.")
 except RuntimeError as e:
     print(f"Erro na análise semântica: {e}")
 
 symbol_table = semantic_analyzer.get_all_symbols()
+sem.show_symbol_table(symbol_table)
 
 print()
-'''
+
 # Gerar código intermediário
 print("Gerador do Código Intermediário...")
 codI = gi.process_node(ast, symbol_table)
@@ -44,8 +44,8 @@ print(codI)
 print("Geração concluída.")
 
 print()
-'''
-'''
+
+
 # Converter código intermediário para MIPS
 print("Convertendo o Código Intermediário para MIPS...")
 mips_code = pmips.process_intermediate_to_mips(codI.split("\n"))
@@ -57,4 +57,3 @@ output_file = "quick_sort.asm"
 with open(output_file, 'w', encoding="utf-8") as f:
     f.write(mips_code)
 print(f"Código MIPS salvo em {output_file}.")
-'''
