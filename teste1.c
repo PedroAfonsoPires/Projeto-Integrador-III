45e17493929bad1e5f60134546bc9dcfc9920762
OtavioFarias <137441117+OtavioFarias@users.noreply.github.com>
2024-12-19 17:57:41 -0300

    Create teste1.c

---
+++ teste1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Função principal
+int main() {
+    int i;
+    float k;
+    i = 0;
+    while(i < 10){
+        i++;
+        if(i == 5){
+            k = 5.0;
+        }
+    }
+    printf(k);
+    return 0;
+}
